17c76dc9260e16ad0b47ce00cc5c62bdb1ca7479
HCH <attig.benyoussefhassenne@gmail.com>
2018-07-12 11:36:10 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PonyComponent } from './pony/pony.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PonyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
